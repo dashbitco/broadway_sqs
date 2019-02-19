3bdeed8dc9261df2f9bb1dd27baf9af92ea23f6c
Marlus Saraiva <marlus.saraiva@gmail.com>
2019-02-19 12:20:20 -0300

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -1,13 +1,20 @@
 defmodule BroadwaySqs.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @description "A SQS connector for Broadway"
+
   def project do
     [
       app: :broadway_sqs,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.5",
+      name: "BroadwaySQS",
+      description: @description,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      package: package()
     ]
   end
 
@@ -22,7 +29,23 @@ defmodule BroadwaySqs.MixProject do
       {:broadway, "~> 0.1"},
       {:ex_aws_sqs, "~> 2.0"},
       {:hackney, "~> 1.9", only: [:dev]},
-      {:sweet_xml, "~> 0.6"}
+      {:sweet_xml, "~> 0.6"},
+      {:ex_doc, ">= 0.19.0", only: :docs}
     ]
+  end
+
+  defp docs do
+    [
+      main: "BroadwaySQS.Producer",
+      source_ref: "v#{@version}",
+      source_url: "https://github.com/plataformatec/broadway_sqs",
+    ]
+  end
+
+  defp package do
+    %{
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/plataformatec/broadway_sqs"}
+    }
   end
 end
