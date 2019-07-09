979f44556edbea1b849fa3532dc1e8ab794fa684
Wojtek Mach <wojtek@wojtekmach.pl>
2019-07-09 16:28:36 +0200

    Rename type variable message to reason

--- lib/broadway_sqs/sqs_client.ex
+++ lib/broadway_sqs/sqs_client.ex
@@ -11,7 +11,7 @@ defmodule BroadwaySQS.SQSClient do
 
   @type messages :: [Message.t()]
 
-  @callback init(opts :: any) :: {:ok, normalized_opts :: any} | {:error, message :: binary}
+  @callback init(opts :: any) :: {:ok, normalized_opts :: any} | {:error, reason :: binary}
 
   @callback receive_messages(demand :: pos_integer, opts :: any) :: messages
 end
