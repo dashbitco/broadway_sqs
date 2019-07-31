81609899e9657a8b4ca0e54371d59e07ec16ca90
Wojtek Mach <wojtek@wojtekmach.pl>
2019-07-31 12:37:09 +0200

    Update examples/sqs_example to use current project

--- examples/sqs_example/mix.exs
+++ examples/sqs_example/mix.exs
@@ -11,7 +11,6 @@ defmodule BroadwaySQSExample.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger],
@@ -19,14 +18,11 @@ defmodule BroadwaySQSExample.MixProject do
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:broadway_sqs, "~> 0.1.0"},
+      {:broadway_sqs, path: "../.."},
       {:hackney, "~> 1.9"},
       {:httpoison, "~> 0.13.0"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
   end
 end
